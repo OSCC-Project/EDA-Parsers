758c9be6e8a9e124d2360e0808136f13461ee3f8
Xingquan-Li <fzulxq@mgmail.com>
2023-11-12 18:25:39 +0800

    update license

--- parser_rust/vcd/VcdParserRustC.hh
+++ parser_rust/vcd/VcdParserRustC.hh
@@ -1,7 +1,7 @@
 // ***************************************************************************************
 // Copyright (c) 2023-2025 Peng Cheng Laboratory
-// Copyright (c) 2023-2025 Institute of Computing Technology, Chinese Academy of
-// Sciences Copyright (c) 2023-2025 Beijing Institute of Open Source Chip
+// Copyright (c) 2023-2025 Institute of Computing Technology, Chinese Academy of Sciences 
+// Copyright (c) 2023-2025 Beijing Institute of Open Source Chip
 //
 // iEDA is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan
